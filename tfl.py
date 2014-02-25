3a682f3b132b2247da31c5f7c4eeec18b7c59b44
Romaine <romainerc@gmail.com>
2014-02-25 13:41:48 +0000

    Got rid of the worthless code and got it to work

--- tfl.py
+++ tfl.py
@@ -4,64 +4,37 @@ import xml.dom.minidom as xDom
 print 'test'
 
 class Bus():
-    apiurl = "http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1?"
+    apiurl = "http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1?stoppointname="
 
     def eta(self, args):
             
-            api = urllib.urlopen("http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1?stoppointname="
-                    + args)
-            print "\n"
+        api = urllib.urlopen(self.apiurl + args)
+        print "\n"
 
-            sysTime = api.readline()
-            sysTime = json.loads(sysTime)
-            sysTime = sysTime[2]
-            
-            lines = api.readlines()
-            for line in lines:
-                    line = json.loads(line)
-                    #print line
-                    bus = line[2]
-                    busTime = line[3]
-                    eta = busTime - sysTime
-                    eta = eta / 1000
-                    eta = eta / 60
+        sysTime = api.readline()
+        print sysTime
+        sysTime = json.loads(sysTime)
+        sysTime = sysTime[2]
+        print sysTime 
+        lines = api.readlines()
+        for line in lines:
+                line = json.loads(line)
+                #print line
+                bus = line[2]
+                busTime = line[3]
+                eta = busTime - sysTime
+                eta = eta / 1000
+                eta = eta / 60
                     
-                    if eta == 0:
-                            eta = "Due"
-                            
-                    bus = str(bus)
-                    eta = str(eta)
-                    display = bus + eta.rjust(len(eta) - len(bus) + 8)
-                    print display
+                if eta == 0:
+                    eta = "Due"
+                        
+                bus = str(bus)
+                eta = str(eta)
+                display = bus + eta.rjust(len(eta) - len(bus) + 8)
+                print display
                     
-    def printer():
-            i = 3
-            road = raw_input("Choose a bus stop: ")
-            api = urllib.urlopen("http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1?stoppointname=" + road)
-            print "\n"
 
-            sysTime = api.readline()
-            sysTime = json.loads(sysTime)
-            sysTime = sysTime[2]
-            
-            lines = api.readlines()
-            for line in lines:
-                    line = json.loads(line)
-                    #print line
-                    bus = line[2]
-                    busTime = line[3]
-                    eta = busTime - sysTime
-                    eta = eta / 1000
-                    eta = eta / 60
-                    
-                    if eta == 0:
-                            eta = "Due"
-                            
-                    bus = str(bus)
-                    eta = str(eta)
-                    display = str(bus) + str(eta).rjust(len(eta) - len(bus) + 8)
-                    print display
-                    
 
     def busTimes():
             api = urllib.urlopen("http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1?stoppointname=barking road").read()
@@ -102,29 +75,4 @@ bus = Bus()
 
 bus.eta("Suffolk Road")
                 
-
-
-        #parser.add_argument('-p','--parse', dest='ptext', action='store_const',
-        #                    const=printer,
-        #                    help='print epochs')
-
-        #parser.add_argument('-l','--lines', dest='line', action='store_const',
-        #                    const=linesStatus,
-        #                    help='print line statuses')
-
-
-        #parser.parse_args()
-        #args.pline()#
-        #print args.accumulate(args.integers)
-        ##if options.verbose:
-        ##
-        ##        print "Mode is set to verbose!"
-        ##
-        ##
-        ##print options.write
-        ##
-        ##print args[0]
-        ##
-        ##print args[1]
-
 
